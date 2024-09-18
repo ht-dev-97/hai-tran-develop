3934fad90047da8782f83801391672a81e4f118e
Hai tran <hai.tran@digitalfortress.dev>
2024-09-18 15:18:03 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-// import { trackEventServer } from "@/lib/trackEventServer"
+// import amplitudeServiceForNode from "@/lib/amplitude"
 
-import amplitudeServiceForNode from "@/lib/amplitude"
 export default async function Home() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const userProperties: any = {
-    user_id: "test@amplitude.com",
-    name: "Hai Tran",
-  }
-  amplitudeServiceForNode.setUser(userProperties)
-  amplitudeServiceForNode.trackEvent(
-    "test@amplitude.com",
-    "Test Amplitude Node V",
-    { test: "hello" }
-  )
-
-  // await trackEventServer("Test Amplitude Node 1", {})
+  // const userProperties: any = {
+  //   user_id: "test@amplitude.com",
+  //   name: "Hai Tran",
+  // }
+  // amplitudeServiceForNode.setUser(userProperties)
+  // amplitudeServiceForNode.trackEvent(
+  //   "test@amplitude.com",
+  //   "Test Amplitude Node V",
+  //   { test: "hello" }
+  // )
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
