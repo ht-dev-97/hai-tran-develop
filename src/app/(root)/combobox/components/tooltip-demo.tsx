f4ec23a02f14d269d70e14f4698a49d59dea8220
Hai tran <hai.tran@digitalfortress.dev>
2024-10-03 09:59:16 +0700

    update demo

--- src/app/(root)/combobox/components/tooltip-demo.tsx
+++ src/app/(root)/combobox/components/tooltip-demo.tsx
@@ -1,16 +1,37 @@
-import React from "react"
+"use client"
+
+import React, { useState } from "react"
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { cn } from "@/lib/utils"
 
 const TooltipDemo = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <TooltipProvider>
-      <Tooltip delayDuration={0}>
-        <TooltipTrigger>Hover</TooltipTrigger>
+    <TooltipProvider delayDuration={0}>
+      <Tooltip open={open}>
+        <TooltipTrigger asChild>
+          <button
+            type="button"
+            className={cn("cursor-pointer")}
+            onClick={() => setOpen(!open)}
+            onMouseEnter={() => setOpen(true)}
+            onMouseLeave={() => setOpen(false)}
+            onTouchStart={() => setOpen(!open)}
+            onKeyDown={(e) => {
+              e.preventDefault()
+              // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+              e.key === "Enter" && setOpen(!open)
+            }}
+          >
+            Hover
+          </button>
+        </TooltipTrigger>
         <TooltipContent side="bottom">
           <p>Add to library</p>
         </TooltipContent>
