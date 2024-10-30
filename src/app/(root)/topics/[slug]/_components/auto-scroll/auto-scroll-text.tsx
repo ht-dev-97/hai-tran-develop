ad76734e9acdb98df531f2af1edc7f55361534cf
Hai tran <hai.tran@digitalfortress.dev>
2024-10-30 14:32:40 +0700

    fix: Fixed auto scroll text

--- src/app/(root)/topics/[slug]/_components/auto-scroll/auto-scroll-text.tsx
+++ src/app/(root)/topics/[slug]/_components/auto-scroll/auto-scroll-text.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react"
 
 const AutoScrollText = () => {
   return (
-    <div className='overflow-hidden relative h-16 border-t border-b'>
-      <div className='text-3xl tracking-[0.1em] uppercase text-black whitespace-nowrap absolute top-1/2 -translate-y-1/2 pl-[150px] animate-auto-scroll-text opacity-0'>
-        We bring you the best products
+    <div className="overflow-hidden relative h-16 border-t border-b">
+      <div className="absolute top-0 bottom-0 flex items-center pl-[100px] animate-auto-scroll-text opacity-0">
+        <span className="text-3xl tracking-[0.1em] uppercase text-black whitespace-nowrap">
+          We bring you the best products
+        </span>
       </div>
-      <div className='text-3xl tracking-[0.1em] uppercase text-black whitespace-nowrap absolute top-1/2 -translate-y-1/2 pl-[150px] animate-auto-scroll-text2 opacity-0'>
-        We bring you the best products
+      <div className="absolute top-0 bottom-0 flex items-center pl-[100px] animate-auto-scroll-text2 opacity-0">
+        <span className="text-3xl tracking-[0.1em] uppercase text-black whitespace-nowrap">
+          We bring you the best products
+        </span>
       </div>
     </div>
   )
