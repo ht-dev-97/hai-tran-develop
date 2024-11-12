6b5dafe8716ffab2764031a3cc2d44560cc11454
Hai tran <hai.tran@digitalfortress.dev>
2024-11-12 15:48:11 +0700

    fix: fixed remove config next-intl

--- src/configs/next-intl/navigation.ts
+++
@@ -1,9 +0,0 @@
-import { createLocalizedPathnamesNavigation } from "next-intl/navigation"
-import { locales, pathnames, localePrefix } from "./index"
-
-export const { Link, getPathname, redirect, usePathname, useRouter } =
-  createLocalizedPathnamesNavigation({
-    locales,
-    pathnames,
-    localePrefix,
-  })
