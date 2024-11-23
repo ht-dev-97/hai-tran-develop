c9720779baa79a14ae0345b3bafe650fed52246d
Hai tran <hai.tran@digitalfortress.dev>
2024-11-23 23:01:14 +0700

    update code

--- src/app/[locale]/(root)/blogs/_container/index.tsx
+++ src/app/[locale]/(root)/blogs/_container/index.tsx
@@ -1,14 +1,7 @@
-'use client'
-
-import getBlogs from '@/lib/markdown/get-blogs'
 import React from 'react'
 
-import BlogList from '../_components/blog-list'
-
 const BlogsContainer = () => {
-  const blogs = getBlogs('blogs')
-
-  return <BlogList blogs={blogs} />
+  return <h2>okok</h2>
 }
 
 export default BlogsContainer
