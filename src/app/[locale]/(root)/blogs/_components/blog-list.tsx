00ca14f0333a63689fee0ba7db28fefde169e206
Hai tran <hai.tran@digitalfortress.dev>
2024-11-23 22:52:19 +0700

    update code

--- src/app/[locale]/(root)/blogs/_components/blog-list.tsx
+++ src/app/[locale]/(root)/blogs/_components/blog-list.tsx
@@ -1,10 +1,5 @@
 'use client'
 
-import { useState } from 'react'
-
-import BlogCard from './blog-card'
-import BlogsSearch from './blog-search'
-
 interface Blog {
   title: string
   cook_time: string
@@ -20,24 +15,11 @@ interface BlogListProps {
 }
 
 const BlogList = ({ blogs }: BlogListProps) => {
-  const [searchVal, setSearchVal] = useState<string>('')
-
-  const filteredBlogs = blogs.filter((blog) =>
-    blog.modified_title.includes(searchVal)
-  )
+  console.log(blogs)
 
   return (
     <section>
-      <BlogsSearch searchVal={searchVal} setSearchVal={setSearchVal} />
-      <h2 className="text-2xl font-bold">Popular Blogs</h2>
-      <div className="space-y-4 mb-10 mt-5">
-        {filteredBlogs.length === 0 && (
-          <p className="text-sm font-medium">No blogs found.</p>
-        )}
-        {filteredBlogs.map((blog, index) => (
-          <BlogCard blog={blog} key={index} />
-        ))}
-      </div>
+      <h2>okok</h2>
     </section>
   )
 }
