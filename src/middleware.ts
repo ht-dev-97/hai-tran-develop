b7aa7d9937fdac929ff69bafd66a6108aff5895d
Hai tran <hai.tran@digitalfortress.dev>
2025-04-04 15:42:46 +0700

    update code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 import { geolocation } from '@vercel/functions'
 import createMiddleware from 'next-intl/middleware'
-import { NextResponse } from 'next/server'
 
 import { routing } from './i18n/routing'
 
@@ -9,8 +8,6 @@ export default clerkMiddleware((auth, req) => {
   if (req.nextUrl.pathname.startsWith('/api')) {
     return
   }
-
-  const url = req.nextUrl
 
   const { country = '' } = geolocation(req)
 
@@ -22,26 +19,6 @@ export default clerkMiddleware((auth, req) => {
 
   // Add x-user-country header
   intlResponse.headers.set('x-user-country', country)
-
-  const queryParams = url.searchParams
-  const hasQueryParams = queryParams.toString() !== ''
-
-  const amplitudeDeviceId = queryParams.get('amplitude_device_id')
-
-  if (amplitudeDeviceId) {
-    // Save amplitude_device_id to cookie
-    intlResponse.cookies.set('amplitude_device_id', amplitudeDeviceId)
-  }
-
-  if (hasQueryParams) {
-    const cleanUrl = new URL(req.url)
-    cleanUrl.search = ''
-    const redirectResponse = NextResponse.redirect(cleanUrl)
-
-    redirectResponse.cookies.getAll().map(({ name, value, ...options }) => {
-      redirectResponse.cookies.set(name, value, options)
-    })
-  }
 
   return intlResponse
 })
